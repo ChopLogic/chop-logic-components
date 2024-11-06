6857e10306f516144c20f7ef0e9693d117fbbc46
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-11-06 16:52:44 +0100

    docs: update text input storybook

--- src/components/inputs/text/types.ts
+++ src/components/inputs/text/types.ts
@@ -8,18 +8,16 @@ export type RegExpWithFlags = {
 export type TextValidationFunction = (input: string) => boolean;
 
 export type ChopLogicTextInputProps = CommonInputProps & {
-  name: string;
-  label: string;
   errorMessage?: string;
   clearable?: boolean;
   readOnly?: boolean;
-  onClear?: () => void;
   type?: 'text' | 'email' | 'password';
   validator?: RegExpWithFlags | TextValidationFunction;
   maxLength?: number;
   placeholder?: string;
   defaultValue?: string;
   autoComplete?: React.HTMLInputAutoCompleteAttribute;
+  onClear?: () => void;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   onBlur?: React.FocusEventHandler<HTMLInputElement>;
   onFocus?: React.FocusEventHandler<HTMLInputElement>;

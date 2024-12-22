11c90d5bbed7f48708f0bbc7d2402435d278ae22
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-12-22 16:07:05 +0100

    docs: add ChopLogicIcon storybook description

--- src/elements/icon/__docs__/Icon.stories.tsx
+++ src/elements/icon/__docs__/Icon.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { ChopLogicIconName } from '@/enums';
+
 import IconExample from './Icon.example.tsx';
 
 const meta: Meta<typeof IconExample> = {
   title: 'Elements/Icon',
   component: IconExample,
+  argTypes: {
+    name: {
+      control: 'select',
+      options: Object.values(ChopLogicIconName),
+    },
+  },
 };
 
 export default meta;

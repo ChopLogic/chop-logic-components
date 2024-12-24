3963b5fbd77b1cbb9781aab24a6f8a6761aa6071
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2024-12-24 18:15:12 +0100

    chore: remove unused css files

--- src/types/components/dialog.ts
+++ src/types/components/dialog.ts
@@ -1,20 +1,9 @@
-import { HTMLAttributes, PropsWithChildren } from 'react';
-
-import { ChopLogicTheme } from '@/types';
+import { PropsWithChildren } from 'react';
 
 import { CommonComponentProps } from './_common';
 
-export type ChopLogicDialogProps = CommonComponentProps &
-  PropsWithChildren & {
-    isOpened: boolean;
-    onClose: () => void;
-    title: string;
-  };
-
-export type DialogLayoutProps = PropsWithChildren &
-  HTMLAttributes<HTMLDivElement> & {
-    title: string;
-    onClose: () => void;
-    isOpened: boolean;
-    theme: ChopLogicTheme;
-  };
+export interface ChopLogicDialogProps extends CommonComponentProps, PropsWithChildren {
+  isOpened: boolean;
+  onClose: () => void;
+  title: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export type { ChopLogicTheme } from './chop-logic-theme.ts';
 export * from './components/alert.ts';
 export * from './components/button.ts';
 export * from './components/checkbox.ts';
-export * from './components/dialog.ts';
+export type { ChopLogicDialogProps } from './components/dialog.ts';
 export * from './components/form.ts';
 export * from './components/grid.ts';
 export * from './components/icon.ts';

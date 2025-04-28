0b2a7b0e1823cf13a5c92ab2373a0cc93dcc8f2a
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-04-28 20:08:59 +0200

    chore: implement scss module for ErrorMessage

--- src/elements/error-message/ErrorMessage.tsx
+++ src/elements/error-message/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ChopLogicTheme } from '@models';
-
-import { StyledErrorMessage } from './ErrorMessage.styled';
+import { getClassName } from '@utils/get-class-name.ts';
+import styles from './ErrorMessage.module.scss';
+import '@styles/themes/light.scss';
 
 type ChopLogicErrorMessageProps = {
   errorId: string;
@@ -18,12 +19,17 @@ const ChopLogicErrorMessage: React.FC<ChopLogicErrorMessageProps> = ({
   message = 'Invalid input',
   visible = false,
   style,
-  theme,
 }) => {
   return (
-    <StyledErrorMessage id={errorId} $visible={visible} data-testid={testId} style={style} $theme={theme} aria-hidden={!visible}>
+    <span
+      id={errorId}
+      className={getClassName([styles.errorMessage, { [styles.errorMessage__visible]: visible }])}
+      data-testid={testId}
+      style={style}
+      aria-hidden={!visible}
+    >
       {message}
-    </StyledErrorMessage>
+    </span>
   );
 };
 

d182badfd762a566ce03d0ebaa6f80e58ab5d12b
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-05-03 12:31:29 +0200

    style: update ChopLogicLoader

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { getChopLogicTheme } from './get-chop-logic-theme';
 export { handleDropdownListKeyPress } from './handle-dropdown-list-key-press';
 export { moveFocusOnElementById } from './move-focus-on-element-by-id';
+export { getClassName } from './get-class-name';

15fe2d8693f6ac659182decf19edfd160b22295a
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-07-13 21:02:17 +0200

    docs: update Tooltip doc file

--- src/atoms/tooltip/__docs__/Tooltip.stories.tsx
+++ src/atoms/tooltip/__docs__/Tooltip.stories.tsx
@@ -2,14 +2,15 @@ import { ChopLogicTooltipContainer } from '@enums';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { TooltipExample } from './Tooltip.example';
+import { ChopLogicTooltip } from '@atoms';
 
-const meta: Meta<typeof TooltipExample> = {
+const meta: Meta<typeof ChopLogicTooltip> = {
   component: TooltipExample,
   title: 'Atoms/Tooltip',
 };
 
 export default meta;
-type Story = StoryObj<typeof TooltipExample>;
+type Story = StoryObj<typeof ChopLogicTooltip>;
 
 export const VisibleOnHover: Story = {
   args: {

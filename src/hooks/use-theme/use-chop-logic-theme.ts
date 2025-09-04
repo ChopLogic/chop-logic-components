fdda56e7bf3979656fd70f9f99765e7009cdc265
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-09-04 17:15:35 +0200

    docs: update introduction section

--- src/hooks/use-theme/use-chop-logic-theme.ts
+++ src/hooks/use-theme/use-chop-logic-theme.ts
@@ -1,4 +1,4 @@
 import { ThemeContext } from '@contexts';
 import { useContext } from 'react';
 
-export const useChopLogicTheme = () => useContext(ThemeContext);
+export const useTheme = () => useContext(ThemeContext);

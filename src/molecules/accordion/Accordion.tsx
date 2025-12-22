ec377276083f388264bee470ccf301c1cd5549ac
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-12-22 18:06:22 +0100

    style: add scss modules for Accordion component

--- src/molecules/accordion/Accordion.tsx
+++ src/molecules/accordion/Accordion.tsx
@@ -1,9 +1,13 @@
 import type { AccordionItemProps, AccordionProps } from '@models';
+import { getClassName } from '@utils';
+import styles from './Accordion.module.scss';
 import { AccordionItem } from './AccordionItem';
 
 export function Accordion({ items, className }: AccordionProps) {
+  const accordionClass = getClassName([styles.accordion, className]);
+
   return (
-    <div className={className}>
+    <div className={accordionClass}>
       {items.map((item: AccordionItemProps) => (
         <AccordionItem key={item.summary} {...item} />
       ))}
--- src/molecules/accordion/AccordionItem.tsx
+++ src/molecules/accordion/AccordionItem.tsx
@@ -1,15 +1,20 @@
 import { Icon } from '@atoms';
 import { IconName } from '@enums';
 import type { AccordionItemProps } from '@models';
+import styles from './AccordionItem.module.scss';
 
 export function AccordionItem({ summary, children }: AccordionItemProps) {
   return (
-    <details>
-      <summary>
+    <details className={styles.details}>
+      <summary className={styles.details_summary}>
         {summary}
-        <Icon name={IconName.ChevronDown} aria-label="Toggle Details" />
+        <Icon
+          name={IconName.ChevronDown}
+          className={styles.details_icon}
+          aria-label="Toggle Details"
+        />
       </summary>
-      <div>{children}</div>
+      <div className={styles.details_content}>{children}</div>
     </details>
   );
 }

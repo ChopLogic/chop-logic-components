a0f8ab09b02a13c1779242dcc3a4700322f226c0
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2026-01-05 11:24:18 +0100

    feat: add useIsHovered hook

---
+++ src/hooks/use-is-hovered/use-is-hovered.ts
@@ -0,0 +1,24 @@
+import type { RefObject } from 'react';
+import { useEffect, useState } from 'react';
+
+export function useIsHovered(ref: RefObject<HTMLElement>, isMounted = true) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element || !isMounted) return;
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    element.addEventListener('mouseenter', handleMouseEnter);
+    element.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      element.removeEventListener('mouseenter', handleMouseEnter);
+      element.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [ref, isMounted]);
+
+  return isHovered;
+}
